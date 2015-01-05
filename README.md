87d95ce2bab0166a7637c4f558e427d70819b980
Eliars <master@master>
2015-01-05 17:07:01 +0900

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+Compile Env -> linux
+Coder -> Ajou. jin seok hee
+
+upload list -> list.txt
+
---
+++ Server/lib/lib.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <error.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
